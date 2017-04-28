4f542bdb3d826deec54434b48228c109d6b9068e
robert974 <robertka@metropolia.fi>
2017-04-28 14:33:35 +0300

    Add files via upload

--- src/liikkuvat/Pyorat.java
+++ src/liikkuvat/Pyorat.java
@@ -1,5 +1,27 @@
 package liikkuvat;
 
-public class Pyorat {
 
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.Port;
+
+public class Pyorat {
+//eteen
+	//taakse
+	private EV3MediumRegulatedMotor kaannosMoottori;
+	private EV3LargeRegulatedMotor  paaMoottori;
+	
+	public Pyorat (Port kaannosPort, Port paaPort){
+		this.kaannosMoottori = new EV3MediumRegulatedMotor(kaannosPort);
+		this.paaMoottori = new EV3LargeRegulatedMotor(paaPort);
+	}
+	
+	public void eteen(int nopeus){
+		this.paaMoottori.setSpeed(nopeus);
+		this.paaMoottori.forward();
+		
+		
+	}
 }
+
