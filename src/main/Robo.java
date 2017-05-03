06f2dbc1dfd2b6c98a73a6be7b48b1919b4b0162
Viljamha <viljamha@metropolia.fi>
2017-05-03 14:16:44 +0300

    Moved the mode selector to its own setup class

--- src/main/Robo.java
+++ src/main/Robo.java
@@ -11,23 +11,7 @@ import lejos.utility.Delay;
 public class Robo {
 	
 	public static void main(String[] args) {
-		
-		LCD.drawString("Jarkko", 0, 0);
-		LCD.drawString("Kuulantyontaja", 0, 1);
-//		LCD.drawString("Esc lopettaa", 0, 3);
-		LCD.drawString("Vasen automaatti", 0, 4);
-		LCD.drawString("Oikea manuaali", 0, 5);
-		
-		int keycode = Button.waitForAnyPress();
-		
-		LCD.clear();
-		
-		if (keycode == Button.ID_LEFT) {
-			BeaconTracker irAnturi = new BeaconTracker(new EV3IRSensor(SensorPort.S1));
-			irAnturi.start();
-		} else if (keycode == Button.ID_RIGHT) {
-			Tankki tankki = new Tankki();
-			tankki.aloita();
-		}
+		Setup aloitus = new Setup();
+		aloitus.start();
 	}
 }
