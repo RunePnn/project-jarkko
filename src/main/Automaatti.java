c39e26813d785f94ef1ad4f562b5e3f54b5e02b8
Viljamha <viljamha@metropolia.fi>
2017-05-04 09:52:04 +0300

    Add files via upload

---
+++ src/main/Automaatti.java
@@ -0,0 +1,62 @@
+package main;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import liikkuvat.Tykki;
+import sensorit.BeaconTracker;
+
+public class Automaatti {
+	
+	private Tykki tykki;
+	private BeaconTracker infrapuna;
+	private float suunta;
+	private float kantama;
+	
+	private int etsintaSuunta;
+	
+	public Automaatti() {
+		this.infrapuna = new BeaconTracker(SensorPort.S1);
+		this.suunta = 0;
+		this.kantama = 0;
+		
+		this.tykki = new Tykki(MotorPort.D, MotorPort.C);
+		this.tykki.start();
+		
+		this.etsintaSuunta = 0;
+		
+	}
+	
+	public void aloita() {
+		while(!Button.ENTER.isDown()) {
+			float seekSample[] = this.infrapuna.getSample();
+			
+			this.suunta = seekSample[0];
+			this.kantama = seekSample[1];
+			
+			LCD.drawString(this.suunta + "            ", 0, 1);
+			LCD.drawString(this.kantama + "            ", 0, 0);
+			
+			if (this.suunta > 2) {
+				this.tykki.pyoritaAlustaaSulavasti(0); // Vasemmalle
+				Button.LEDPattern(3);
+			} else if (this.suunta < -2) {
+				this.tykki.pyoritaAlustaaSulavasti(1); // Oikealle
+				Button.LEDPattern(3);
+			} else if (this.suunta == 0 && this.kantama == Float.POSITIVE_INFINITY) {
+				if (this.tykki.alusta.getPosition() > 60) {
+					this.etsintaSuunta = 1;
+				} else if (this.tykki.alusta.getPosition() < -60) {
+					this.etsintaSuunta = 0;
+				}
+				this.tykki.pyoritaAlustaaSulavasti(this.etsintaSuunta);
+				Button.LEDPattern(0);
+			} else {
+				this.tykki.lopetaAlustanPyoriminen();
+				Button.LEDPattern(1);
+			}
+		}
+		this.tykki.lopeta();
+	}
+}
--- src/sensorit/BeaconTracker.java
+++ src/sensorit/BeaconTracker.java
@@ -3,6 +3,7 @@ package sensorit;
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.utility.Delay;
@@ -10,44 +11,18 @@ import liikkuvat.Tykki;
 
 public class BeaconTracker extends Thread {
 	private EV3IRSensor sensor;
-	private float direction;
-	private Tykki tykki;
+	private float seekSample[];
 	
-	private boolean paalla;
-	
-	public BeaconTracker(EV3IRSensor sensor) {
-		this.sensor = sensor;
-		this.direction = 0;
+	public BeaconTracker(Port portti) {
+		this.sensor = new EV3IRSensor(portti);
 		
-		this.tykki = new Tykki(MotorPort.D, MotorPort.B);
-		this.tykki.start();
-		
-		this.paalla = true;
-	}
-	
-	public void run() {
 		this.sensor.setCurrentMode(1);
-		while(!Button.ESCAPE.isDown()) {
-			
-			SensorMode seek = this.sensor.getSeekMode();
-			float seekSample[] = new float[seek.sampleSize()];
-			
-			this.sensor.fetchSample(seekSample, 0);
-			this.direction = seekSample[0];
-			
-			LCD.drawString("" + this.direction, 0, 1);
-			if (this.direction > 2) {
-				this.tykki.pyoritaAlustaaSulavasti(0); // Vasemmalle
-			} else if (this.direction < -2) {
-				this.tykki.pyoritaAlustaaSulavasti(1); // Oikealle
-			} else {
-				this.tykki.lopetaAlustanPyoriminen();
-			}
-		}
+		SensorMode seek = this.sensor.getSeekMode();
+		this.seekSample = new float[seek.sampleSize()];
 	}
 	
-	public void lopeta() {
-		this.tykki.lopeta();
-		this.paalla = false;
+	public float[] getSample() {
+		this.sensor.fetchSample(seekSample, 0);
+		return seekSample;
 	}
 }
