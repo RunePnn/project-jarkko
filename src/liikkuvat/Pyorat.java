53a616f5d8619728592b0f5bd8bf7bd7d0bbd6e9
robert974 <robertka@metropolia.fi>
2017-05-08 14:04:09 +0300

    Update Pyorat.java

--- src/liikkuvat/Pyorat.java
+++ src/liikkuvat/Pyorat.java
@@ -11,7 +11,7 @@ public class Pyorat extends Thread {
 	
 	/**
 	 * Luokka, joka ohjaa robotin ajamiseen kayttamia moottoreita.
-	 * @author Robertka
+	 * @author robertka
 	 * @param kaannosPort Kaannosmoottorin porttiasema
 	 * @param paaPort Paamoottorin porttiasema
 	 */
