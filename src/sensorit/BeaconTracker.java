d3ff9b015ee2d50b2f19da7f508f585e44377ed0
Viljamha <viljamha@metropolia.fi>
2017-05-08 11:58:06 +0300

    Update BeaconTracker.java

--- src/sensorit/BeaconTracker.java
+++ src/sensorit/BeaconTracker.java
@@ -1,17 +1,19 @@
 package sensorit;
 
-import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.SensorMode;
-import lejos.utility.Delay;
-import liikkuvat.Tykki;
 
 public class BeaconTracker extends Thread {
 	private EV3IRSensor sensor;
 	private float seekSample[];
+	
+	private boolean running;
+	
+	/**
+	 * Luokka, joka vastaanottaa beaconin sijainnin ja kantaman.
+	 * @param portti Anturin portti, jossa Infrapuna-anturi on
+	 */
 	
 	public BeaconTracker(Port portti) {
 		this.sensor = new EV3IRSensor(portti);
@@ -19,10 +21,33 @@ public class BeaconTracker extends Thread {
 		this.sensor.setCurrentMode(1);
 		SensorMode seek = this.sensor.getSeekMode();
 		this.seekSample = new float[seek.sampleSize()];
+		
+		this.running = true;
 	}
+	
+	@Override
+	public void run() {
+		while(isRunning()) {
+			this.sensor.setCurrentMode(1);
+			this.sensor.fetchSample(seekSample, 0);
+		}
+		this.sensor.close();
+ 	}
+	
+	/**
+	 * Ottaa näytteen anturin antamasta tiedosta.
+	 */
 	
 	public float[] getSample() {
 		this.sensor.fetchSample(seekSample, 0);
 		return seekSample;
+	}
+	
+	public boolean isRunning() {
+		return this.running;
+	}
+	
+	public void terminate() {
+		this.running = false;
 	}
 }
